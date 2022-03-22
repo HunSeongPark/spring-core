912d10d58b17d46b5205204e4079ac4d7419ab6e
Hunseong <phunseong@gmail.com>
2022-03-23 02:07:14 +0900

    주문, 할인 도메인 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Created by Hunseong on 2022/03/23
+ */
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+
+        // given
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
