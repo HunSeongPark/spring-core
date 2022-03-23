a3970c0a7b15054d439643a8c87d28d4beb72618
Hunseong <phunseong@gmail.com>
2022-03-23 19:08:55 +0900

    request Scope를 사용하는 MyLogger 구현

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Hunseong on 2022/03/23
+ */
+@RequiredArgsConstructor
+@Controller
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
